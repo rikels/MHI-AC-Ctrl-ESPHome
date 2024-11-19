c2ef08b5b8b249947ba07309a16169628a8fd218
Rikels <knakel2@gmail.com>
2024-11-19 21:00:19 +0100

    Update __init__.py

--- components/MhiAcCtrl/__init__.py
+++ components/MhiAcCtrl/__init__.py
@@ -9,6 +9,9 @@ CONF_FRAME_SIZE = 'frame_size'
 CONF_ROOM_TEMP_TIMEOUT = 'room_temp_timeout'
 CONF_VANES_UD = 'vanes_ud'
 CONF_VANES_LR = 'vanes_lr'
+CONF_SCK_PIN = 'sck_pin'
+CONF_MOSI_PIN = 'mosi_pin'
+CONF_MISO_PIN = 'miso_pin'
 
 mhiacctrl = cg.esphome_ns.namespace('mhiacctrl')
 MhiAcCtrl = cg.global_ns.class_('MhiAcCtrl', cg.Component, climate.Climate)
@@ -19,6 +22,9 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_ROOM_TEMP_TIMEOUT, default=60): cv.int_range(min=0, max=3600),
     cv.Optional(CONF_VANES_UD, default=0): cv.int_range(min=0, max=5),
     cv.Optional(CONF_VANES_LR, default=0): cv.int_range(min=0, max=8),
+    cv.Optional(CONF_SCK_PIN, default=14): pins.internal_gpio_output_pin_number,
+    cv.Optional(CONF_MOSI_PIN, default=13): pins.internal_gpio_output_pin_number,
+    cv.Optional(CONF_MISO_PIN, default=12): pins.internal_gpio_output_pin_number,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -28,4 +34,7 @@ async def to_code(config):
     cg.add(var.set_room_temp_api_timeout(config[CONF_ROOM_TEMP_TIMEOUT]))
     cg.add(var.set_vanes(config[CONF_VANES_UD]))
     cg.add(var.set_vanesLR(config[CONF_VANES_LR]))
+    cg.add(var.set_vanesLR(config[CONF_SCK_PIN]))
+    cg.add(var.set_vanesLR(config[CONF_MOSI_PIN]))
+    cg.add(var.set_vanesLR(config[CONF_MISO_PIN]))
     cg.add(var.get_binary_sensors())
